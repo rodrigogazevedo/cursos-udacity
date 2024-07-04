a5a981a49438121a2d7a06b814f0512f50bb8bec
Rodrigo Guimarães de Azevedo <rodrigogdeazevedo@gmail.com>
2024-07-03 23:46:27 -0300

    adding the number of likes, likes button and likes counter to the photo

--- react/React-Fundamentals-Exercises/L3. State Management/3. Managing State/Starter/src/App.js
+++ react/React-Fundamentals-Exercises/L3. State Management/3. Managing State/Starter/src/App.js
@@ -1,12 +1,16 @@
+import { useState } from 'react';
 import corgi from "./corgi.jpg";
 import "./App.css";
 
 const App = () => {
+  const [numberOfLikes, setNumberOfLikes] = useState(0);
+
   return (
     <div className="container">
       <h2>Like this photo!</h2>
       <img src={corgi} alt="Corgi"></img>
-      <p>Amount of likes: </p>
+      <p>Amount of likes: {numberOfLikes}</p>
+      <button onClick={() => setNumberOfLikes(numberOfLikes + 1)}>Like</button>
     </div>
   );
 };
